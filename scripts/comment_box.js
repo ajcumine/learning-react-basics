576d80c12c240146f9169911f8f9b59777038fd4
ajcumine <ajcumine@gmail.com>
2014-10-10 14:05:07 +0100

    Moved server stored comments to /comments.json url

--- scripts/comment_box.js
+++ scripts/comment_box.js
@@ -1,11 +1,30 @@
 /** @jsx React.DOM */
 
 var CommentBox = React.createClass({
+  loadCommentsFromServer: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  getInitialState: function() {
+    return {data: []};
+  },
+  componentDidMount: function() {
+    this.loadCommentsFromServer();
+    setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+  },
   render: function() {
     return (
       <div className="commentBox">
         <h1>Comments</h1>
-        <CommentList data={this.props.data} />
+        <CommentList data={this.state.data} />
         <CommentForm />
       </div>
     );
@@ -55,12 +74,7 @@ var Comment = React.createClass({
   }
 });
 
-var data = [
-  {author: "Pete Hunt", text: "This is one comment"},
-  {author: "Jordan Walke", text: "This is *another* comment"}
-];
-
 React.renderComponent(
-  <CommentBox data={data} />,
+  <CommentBox url="comments.json" pollInterval= {2000} />,
   document.getElementById('content')
 );
--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 var express = require('express');
 var app = express();
 
+var comments = [
+  {author: "Pete Hunt", text: "This is one comment"},
+  {author: "Jordan Walke", text: "This is *another* comment"}
+];
 
 app.set('view engine', 'jade');
 app.use(express.static(__dirname));
 
 app.get('/', function(req, res) {
   res.render('index');
+});
+
+app.get('/comments.json', function(req, res) {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify(comments));
+});
+
+app.post('/comments.json', function(req, res) {
+  comments.push(req.body);
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify(comments));
 });
 
 app.listen(3000);
