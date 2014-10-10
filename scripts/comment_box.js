7feb8ec8f4988e73808ebf0beafc483a3ead93f0
ajcumine <ajcumine@gmail.com>
2014-10-10 12:24:52 +0100

    Stored comments are now in JSON

--- scripts/comment_box.js
+++ scripts/comment_box.js
@@ -5,7 +5,7 @@ var CommentBox = React.createClass({
     return (
       <div className="commentBox">
         <h1>Comments</h1>
-        <CommentList />
+        <CommentList data={this.props.data} />
         <CommentForm />
       </div>
     );
@@ -14,10 +14,16 @@ var CommentBox = React.createClass({
 
 var CommentList = React.createClass({
   render: function() {
+    var commentNodes = this.props.data.map(function (comment) {
+      return (
+        <Comment author={comment.author}>
+          {comment.text}
+        </Comment>
+      );
+    });
     return (
       <div className="commentList">
-        <Comment author="Pete Hunt">This is one comment</Comment>
-        <Comment author="Jordan Walke">This is *another* comment</Comment>
+        {commentNodes}
       </div>
     );
   }
@@ -49,7 +55,12 @@ var Comment = React.createClass({
   }
 });
 
+var data = [
+  {author: "Pete Hunt", text: "This is one comment"},
+  {author: "Jordan Walke", text: "This is *another* comment"}
+];
+
 React.renderComponent(
-  <CommentBox />,
+  <CommentBox data={data} />,
   document.getElementById('content')
 );
